aa37c1829fcbac7de5adb5187f476236e662401b
chunchanil67-maker <chunchanil67@gmail.com>
2026-06-18 09:19:01 +0900

    AutoPilot: update data/site-config.js

---
+++ data/site-config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  "siteName": "Insurance Advisor US",
+  "primaryColor": "#047857",
+  "bgMain": "#f0fdf4",
+  "fontHeading": "'Plus Jakarta Sans'",
+  "layoutStyle": "modern"
+};
